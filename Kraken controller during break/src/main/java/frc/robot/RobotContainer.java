342514bcd5b9b49ef92d49fae8f25f9b39c37c47
c-amber <chi779060@sas.edu.sg>
2025-11-15 09:47:54 +0800

    Update RobotContainer.java

--- Kraken controller during break/src/main/java/frc/robot/RobotContainer.java
+++ Kraken controller during break/src/main/java/frc/robot/RobotContainer.java
@@ -3,29 +3,26 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.MotorSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 
 public class RobotContainer {
 
   private final MotorSubsystem m_motorSubsystem = new MotorSubsystem();
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandPS4Controller m_driverController =
+      new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
 
   public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
-    m_driverController.a().whileTrue(m_motorSubsystem.spinMotorCommand(1));
-    m_driverController.b().whileTrue(m_motorSubsystem.spinMotorCommand(-1));
-    m_driverController.x().whileTrue(m_motorSubsystem.spinMotorCommand(3));
-    m_driverController.y().whileTrue(m_motorSubsystem.spinMotorCommand(-3));
+    m_driverController.cross().whileTrue(m_motorSubsystem.spinMotorCommand(1));
+    m_driverController.circle().whileTrue(m_motorSubsystem.spinMotorCommand(-1));
+    m_driverController.square().whileTrue(m_motorSubsystem.spinMotorCommand(3));
+    m_driverController.triangle().whileTrue(m_motorSubsystem.spinMotorCommand(-3));
   }
 
   public Command getAutonomousCommand() {
     return null;
   }
 }
-
-
-
