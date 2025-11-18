35fd5da78023864d810adf8ead2679328af3d1e8
harry <harryjaewon@gmail.com>
2025-11-18 16:44:44 +0800

    fixed controller port, changed to ps5 from 4, increased motorspeed arguments, and modified some libraries

--- Kraken controller during break/src/main/java/frc/robot/RobotContainer.java
+++ Kraken controller during break/src/main/java/frc/robot/RobotContainer.java
@@ -1,25 +1,25 @@
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+// import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.MotorSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 
 public class RobotContainer {
 
   private final MotorSubsystem m_motorSubsystem = new MotorSubsystem();
-  private final CommandPS4Controller m_driverController =
-      new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
+  private final CommandPS5Controller m_driverController =
+      new CommandPS5Controller(0);
 
   public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
-    m_driverController.cross().whileTrue(m_motorSubsystem.spinMotorCommand(1));
-    m_driverController.circle().whileTrue(m_motorSubsystem.spinMotorCommand(-1));
-    m_driverController.square().whileTrue(m_motorSubsystem.spinMotorCommand(3));
-    m_driverController.triangle().whileTrue(m_motorSubsystem.spinMotorCommand(-3));
+    m_driverController.cross().whileTrue(m_motorSubsystem.spinMotorCommand(8));
+    m_driverController.circle().whileTrue(m_motorSubsystem.spinMotorCommand(-8));
+    m_driverController.square().whileTrue(m_motorSubsystem.spinMotorCommand(6));
+    m_driverController.triangle().whileTrue(m_motorSubsystem.spinMotorCommand(-6));
   }
 
   public Command getAutonomousCommand() {
